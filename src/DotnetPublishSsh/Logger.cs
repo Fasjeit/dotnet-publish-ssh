736602a6cdef46e6594e8507aa28b19943ddf091
Fasjeit <15075638+Fasjeit@users.noreply.github.com>
2024-10-31 21:41:15 +0300

    Add versioning

---
+++ src/DotnetPublishSsh/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DotnetPublishSsh
+{
+    internal static class Logger
+    {
+        public static LogLevel Enable { get; set; } = LogLevel.Info;
+
+        public static void WriteInfoLine()
+        {
+            Logger.WriteInfoLine(string.Empty);
+        }
+
+        public static void WriteInfoLine(string str)
+        {
+            if (Logger.Enable.HasFlag(LogLevel.Info)) { Console.WriteLine(str); }
+        }
+
+        public static void WriteErrorLine(string str)
+        {
+            if (Logger.Enable.HasFlag(LogLevel.Error)) { Console.WriteLine(str); }
+        }
+    }
+
+    [Flags]
+    internal enum LogLevel
+    {
+        NoLogs = 0,
+        Error = 1,
+        Info = 1<<1 | LogLevel.Error,
+    }
+}
